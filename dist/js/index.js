8451cf8065edf93d8d0198e22727c882e1e7e827
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-03-04 18:42:16 -0300

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const x = 1912;
+console.log(x);
+let tuple;
+tuple = ["Irineu", 10, true, [20, 30]];
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const x:number = 1912
+
+console.log(x)
+
+let tuple: [string, number, boolean, number[]]
+
+tuple = ["Irineu", 10, true, [20, 30]]
