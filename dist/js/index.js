5fbc260624703396a012b22cb8356b9b6bfa21d1
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-03-05 17:43:10 -0300

    a

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,3 +3,43 @@ const x = 1912;
 console.log(x);
 let tuple;
 tuple = ["Irineu", 10, true, [20, 30]];
+let object = {
+    name: "Serjao",
+    age: 45
+};
+console.log(object.name);
+//union type
+let id = "abc123efg456";
+id = 987654;
+console.log(id);
+let newId = "xyz";
+console.log(newId);
+var Size;
+(function (Size) {
+    Size["P"] = "pequeno";
+    Size["M"] = "m\u00E9dio";
+    Size["G"] = "grande";
+})(Size || (Size = {}));
+const camisa = {
+    name: "camisa gola v",
+    size: Size.M
+};
+//literal types
+let teste;
+teste = "autenticado";
+teste = null;
+//funcoes
+function sum(a, b) {
+    return a + b;
+}
+console.log(Number(sum(0.1, 0.2).toFixed(1)) === 0.3);
+//argumento opcional
+function hello(firstName, lastName) {
+    lastName ? console.log(`Hello ${firstName} ${lastName}`) : console.log(`Hello ${firstName}`);
+}
+hello("João");
+hello("Napoleão", "Bonaparte");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,66 @@ const x:number = 1912
 console.log(x)
 
 let tuple: [string, number, boolean, number[]]
-
 tuple = ["Irineu", 10, true, [20, 30]]
+
+let object: {name: string, age: number} = {
+    name: "Serjao",
+    age: 45
+}
+
+console.log(object.name)
+
+//union type
+let id: string | number = "abc123efg456"
+id = 987654
+
+console.log(id)
+
+//type alias
+type myType = number | string
+let newId: myType = "xyz"
+
+console.log(newId)
+
+enum Size {
+    P = "pequeno",
+    M = "médio",
+    G = "grande"
+}
+
+const camisa = {
+    name: "camisa gola v",
+    size: Size.M
+}
+
+//literal types
+let teste: "autenticado" | null
+
+teste = "autenticado"
+teste = null
+
+//funcoes
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+console.log(Number(sum(0.1, 0.2).toFixed(1)) === 0.3)
+
+//argumento opcional
+function hello(firstName: string, lastName?: string): void {
+    lastName ? console.log(`Hello ${firstName} ${lastName}`) : console.log(`Hello ${firstName}`)
+}
+
+hello("João")
+hello("Napoleão", "Bonaparte")
+
+interface MathFunctionParams {
+    n1: number,
+    n2: number
+}
+
+function sumNumbers(nums: MathFunctionParams) {
+    return nums.n1 + nums.n2;
+}
+
+console.log(sumNumbers({ n1: 1, n2: 2 }))
